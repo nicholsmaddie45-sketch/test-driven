7904f49ca04534fc9c64047ca97d624a6dee5779
Madeliene <nicholsmaddie45@gmail.com>
2026-04-23 14:34:43 -0600

    i feel good about this but some of my answers were wrong even when i thought they would work

--- index.js
+++ index.js
@@ -12,8 +12,16 @@
  * sumDouble(5, 5); // 20
  */
 export function sumDouble(num1, num2) {
-  /* TODO */
+  const sum = num1 + num2;
+  return num1 === num2 ? sum * 2 : sum;
 }
+/**const sum = num1 + num2;
+  if (num1 + num2) {
+    return sum;
+  }
+  if (num1 + num2 != sum) {
+    return num1 + num2 * 2;
+  }*/
 
 /**
  * @param {number} num1 - First number.
@@ -28,8 +36,17 @@ export function sumDouble(num1, num2) {
  * makes10(4, 4); // false
  */
 export function makes10(num1, num2) {
-  /* TODO */
+  return num1 === 10 || num2 === 10 || num1 + num2 === 10;
 }
+
+/**export function makes10(num1, num2) {
+  if (num1 === 10 || num2 === 10) {
+    return true;
+  }
+  if (num1 + num2 === 10) {
+    return true;
+  }
+}*/
 
 /**
  * Hint: Math.abs(num) returns the absolute value of num.
@@ -46,9 +63,9 @@ export function makes10(num1, num2) {
  * near100(105, 10); // true
  */
 export function near100(n, distance) {
-  /* TODO */
+  return Math.abs(100 - n) <= distance;
+  /** I didnt rlly understand this one (ask abt math.abs distance) */
 }
-
 /**
  * Hint: use the remainder operator (%)
  *
@@ -63,7 +80,16 @@ export function near100(n, distance) {
  * isMultiple35(7); // false
  */
 export function isMultiple35(n) {
-  /* TODO */
+  if (n % 3 === 0) {
+    return true;
+  }
+  if (n % 5 === 0) {
+    return true;
+  }
+  if (n % 10 === 0) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -81,9 +107,8 @@ export function isMultiple35(n) {
  * shareLastDigit(10, 21); // false
  */
 export function shareLastDigit(num1, num2) {
-  /* TODO */
+  return num1 % 10 === num2 % 10;
 }
-
 /**
  * @param {number} temp1 - First temperature.
  * @param {number} temp2 - Second temperature.
@@ -98,8 +123,15 @@ export function shareLastDigit(num1, num2) {
  * isColdAndHot(10, 50); // false
  */
 export function isColdAndHot(temp1, temp2) {
-  /* TODO */
+  return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
 }
+
+/*export function isColdAndHot(temp1, temp2) {
+  if (temp1 < 0 && temp2 > 100) {
+    return true;
+  }
+  if (temp1 > 100 && temp2 < 0) return true;
+}*/
 
 /**
  * @param {string} A - First string.
@@ -114,7 +146,7 @@ export function isColdAndHot(temp1, temp2) {
  * makeABBA("a", "b"); // "abba"
  */
 export function makeABBA(A, B) {
-  /* TODO */
+  return A + B + B + A;
 }
 
 /**
@@ -135,7 +167,7 @@ export function makeABBA(A, B) {
  * makeSLS("a", "abc"); // "aabca"
  */
 export function makeSLS(str1, str2) {
-  /* TODO */
+  return str1.length < str2.length ? str1 + str2 + str1 : str2 + str1 + str2;
 }
 
 /**
@@ -156,8 +188,26 @@ export function makeSLS(str1, str2) {
  * canEnterClub(5, 5); // 1
  */
 export function canEnterClub(you, date) {
-  /* TODO */
+  if (you <= 2 || date <= 2) {
+    return 0;
+  } else if (you >= 8 || date >= 8) {
+    return 2;
+  } else {
+    return 1;
+  }
 }
+
+/*export function canEnterClub(you, date) {
+  if (you && date >= 8) {
+    return 2;
+  }
+  if (you || date < 2) {
+    return 0;
+  }
+  if (date >= 8 && you < 8) {
+    return 1;
+  }
+}*/
 
 /**
  * Your phone rings. Normally you answer, except:
@@ -177,5 +227,20 @@ export function canEnterClub(you, date) {
  * shouldAnswerPhone(true, true, false); // true
  */
 export function shouldAnswerPhone(isMorning, isBoss, isAsleep) {
-  /* TODO */
+  if (isAsleep) {
+    return false;
+  } else if (isMorning) {
+    return isBoss;
+  } else {
+    return true;
+  }
 }
+
+/*export function shouldAnswerPhone(isMorning, isBoss, isAsleep) {
+  if (isMorning && isBoss) {
+    return true;
+  }
+  if (isAsleep) {
+    return false;
+  }
+}*/
